b32ee9d4b488146ca897f84acf05933721367f19
stevenbr <stevenbrodriguez@gmail.com>
2016-11-18 23:11:40 -0800

    some updates

--- read_create.c
+++ read_create.c
@@ -6,17 +6,12 @@
 /*   By: srodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/10 23:49:34 by srodrigu          #+#    #+#             */
-/*   Updated: 2016/11/18 00:11:03 by srodrigu         ###   ########.fr       */
+/*   Updated: 2016/11/18 23:06:43 by srodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./headers/fillit.h"
 #include "./headers/libft.h"
-
-//int is_validchar()
-//{
-//	
-//}
 
 void create_tetris_list(char *str, t_list **list, int nbr_pieces)
 {
@@ -24,11 +19,13 @@ void create_tetris_list(char *str, t_list **list, int nbr_pieces)
 	char *tetri;
 	
   	ft_putnbr(nbr_pieces);
+	//tetris pieces will have all of the characters until we us ft_trim_newline
 	while(*str && (tetri = (char *)malloc(sizeof(char) * 21)) && !(count = 0))
 	{
 		*(tetri + (20)) = '\0';
 		while ((count < 20) && (*str))
 			*(tetri + count++) = *(str++);
+		//ft_trim_newline returns char *, we use the functions return in the list.
 		ft_list_push_back(list, ft_trim_newline(tetri));
 		str++;
 	}
