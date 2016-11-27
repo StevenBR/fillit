54926e989df7d708b451590cdd898456a30bb46d
Joanna Jia <jjia25@gmail.com>
2016-11-27 05:32:43 -0800

    list again

--- list_pre_check.c
+++ list_pre_check.c
@@ -12,61 +12,78 @@
 
 #include "./headers/fillit.h"
 
-char *copy_lst_content(t_list *list, char **fourbyfourboard)
+int		can_place(char **tetrimino, char **board, int size, t_pos pos)
+{
+	int x;
+	int y;
+	int emptyspot;
+
+	x = 0;
+	y = 0;
+	emptyspot = 0;
+	size = board[x];
+	while (y < 4)
+	{
+		while (x < 4)
+		{
+			if (tetrimino[y][x] == '#')
+			{
+				if (board[pos.row + y][pos.col + x] != '#')
+					emptyspot++;
+			}
+			x++;
+		}
+		y++;
+	}
+	if (emptyspot == 4)
+		return (1);
+	return (0);
+}
+
+char *place_tet(char **tetrimino, char **board, int size)
 {
 	int x;
 	int y;
 
 	x = 0;
 	y = 0;
-	data = 0;
-	while (list)
+	while (y < size)
 	{
-		while (y <= 3)
+		while (x < size)
 		{
-			while (x <= 3)
-			{
-				if (data == "#")
-					ft_strcpy((fourbyfourboard[y][x]), data[x]);
-				else
-					data++;
-				x++;
-			}
-			y++;
+			if (tetrimino[y][x] == '#')
+				board[y][x] = '#';
+			x++;
 		}
+		y++;
 	}
 }
 
-char **create_initial_board(void) //creates initial board to fit first piece
+char **create_board(int size)
 {
-	char **fourbyfourboard;
+	char **board;
 	char *rows;
 	int x;
 	int y;
 
 	x = 0;
 	y = 0;
-	if (!(fourbyfourboard = (char **)malloc(sizeof(char *) * 4)))
+	if (!(board = (char **)malloc(sizeof(char *) * size)))
 		return (0);
-	while (y <= 3)
+	while (y <= size)
 	{
-		while (x < 4)
+		while (x < size)
 		{
-			if (!(rows = (char *)malloc(sizeof(char) * 5)))
+			if (!(rows = (char *)malloc(sizeof(char) * size)))
 				return (0);
 			rows[x] = '.';
 			ft_putchar(rows[x]);
-			if (x == 3)
-			{
-				rows[x] = '\n';
-				ft_putchar(rows[x]);
-			}
 			x++;
 		}
 		x = 0;
 		y++;
 	}
-	return (fourbyfourboard);
+	return (board);
 }
 
 /*
