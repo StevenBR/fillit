5ee26d1b379197eb1c59bf98882536027455acb5
Joanna Jia <jjia25@gmail.com>
2016-11-28 00:24:04 -0800

    main 11/28

--- main.c
+++ main.c
@@ -22,51 +22,71 @@ int main(int ac, char **av)
 	t_list *curr;
 	int	num_tets;
 	int size;
-	char ***tetriminos;
+	char ***alltetriminos;
+	char **board;
+	t_pos pos;
 
-	
+	pos.row = 0;
+	pos.col = 0;
 	list = 0;
 	if (ac != 2)
 	{
-		(ft_putstr("error"));
+		(ft_putstr("1) error"));
 		return (1);
 	}
 	else
 	{
-		file_read_create(av[1], &list);
+		file_read_create(av[1], &list); //stores everything we read in a list, each node is 20 to 21 chars
 		num_tets = 0;
 		curr = list;
-		while (curr)
+		while (curr) //counts number of tets
 		{
-			printf("%s\n", curr->data);
 			num_tets++;
 			curr = curr->next;
 		}
-		printf("mallocking\n");
-		tetriminos = (char ***)malloc(sizeof(char **) * num_tets);
-		num_tets = 0;
+		if (!(alltetriminos = (char ***)malloc(sizeof(char **) * num_tets)))
+			return (0);
 		curr = list;
-		printf("malloc completed\n");
+		num_tets = 0; //being used as an index
 		while (curr)
 		{
-			tetriminos[num_tets] = convert_1d_to_2d(list->data);
-			shift_tetrimino(tetriminos[num_tets]);
+			printf("1) num_tets is %d\n", num_tets);
+			alltetriminos[num_tets] = convert_1d_to_2d(curr->data); //store each node in num_tets, part of 3d array
+			shift_tetrimino(alltetriminos[num_tets]); //validate tetriminos
 			int i = 0;
 			while (i < 4)
 			{
-				printf("%s\n", tetriminos[num_tets][i++]);
+				printf("Shifted: %s\n", alltetriminos[num_tets][i++]);
 			}
-			if(is_valid_tetrimino(tetriminos[num_tets]) == 0)
+			if(is_valid_tetrimino(alltetriminos[num_tets]) == 0)
 			{
-				ft_putstr("error");
+				ft_putstr("2) error");
 				return (1);
 			}
+			num_tets++;
 			curr = curr->next;
-		}
+		} //work out of alltetriminos from this point forward
+		printf("2) num_tets is %d\n", num_tets);
 		size = 2;
-		while (size * size < 4 * num_tets)
-			size++;
-		create_board(size);
+		board = size_map(size, num_tets); //create initial board based on num_tets
+		//board = create_board(size);
+		num_tets = 0;
+		// while (alltetriminos[num_tets] != '\0')
+		// {
+			
+			printf("%d\n", solve(alltetriminos[num_tets], board, size, pos, num_tets));
+		// 	num_tets++;
+		// }
+		// while (curr)
+		// {
+		// 	ft_putstr("Original map:\n");
+		// 	print_map(board, size);
+		// 	if (can_place(alltetriminos[num_tets], board, size, pos) == 1)
+		// 	{
+		// 		place_tet(alltetriminos[num_tets], board, size);
+		// 	}
+		// 	curr = curr->next;
+		// }
 		// create_initial_board();
 	}
 	// (list_is_valid(list)) ? ft_putstr("valid list: :D\n")
